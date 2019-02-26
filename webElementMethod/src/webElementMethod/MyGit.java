a2e1fd74961fa6426de514ed99333199c2e0584a
nsneeraj <46974049+nsneeraj@users.noreply.github.com>
2019-02-26 16:49:51 +0530

    hii

--- webElementMethod/src/webElementMethod/MyGit.java
+++ webElementMethod/src/webElementMethod/MyGit.java
@@ -1,14 +1,20 @@
 package webElementMethod;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class MyGit {
-@Test
+static {
+	System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
 	
-		
+}
+	@Test
+	
 		public void display() {
 			
-		
+		WebDriver driver = new ChromeDriver();
+		driver.quit();
 	}
 
 }
